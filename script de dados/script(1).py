7fd87a559c4d811f596d383c6821230f05d58a3c
Arthur Tavares <arthur.valim@sou.unifeob.edu.br>
2025-04-04 12:00:00 -0300

    ventos e chuvas

--- script de dados/script(1).py
+++ script de dados/script(1).py
@@ -1,6 +1,5 @@
 import csv
 import random
-import time
 
 idRio = 0 #Identificador do rio, servindo como nome
 mediaVazao = 0 #Vazão media/padrão do rio mediada em m3/s
@@ -102,81 +101,3 @@ with open('dadosAlagamentoPI.csv', 'w', newline='') as csvfile:
     print('Escrito')
     writer = csv.writer(csvfile)
     writer.writerows(data)
-
-while True:
-    print('Dormindo (zzz)')
-    time.sleep(30)
-    for idRio in range(10000):
-        mediaVazao = round(random.triangular(2000, 200000, 5000)) #Gera a vazão media do rio
-
-        if random.random() < 0.05: #Cinco porcento de chance de gerar uma vazão errada
-            vazao = round(random.triangular(mediaVazao*50, mediaVazao*200, mediaVazao*100))
-        else:
-            vazao = round(random.triangular(mediaVazao/2, mediaVazao*2, mediaVazao)) #Gera a vazão atual do rio baseando na vazão media
-
-        if random.random() < 0.5: #50 porcento de chance de estar chovendo
-            miliHora = round(random.triangular(0, 65, 5))
-        else:
-            miliHora = 0
-
-        miliDia = round(random.triangular(0, 100, 0))
-        mili7= round(random.triangular(0, 500, 4))
-
-        if random.random() < 0.05: #Cinco porcento de chance de gerar uma temperatura errada
-            temp = round(random.triangular(70, 500, 300))
-        else:
-            temp = round(random.triangular(-5, 40, 24))
-
-        if miliHora >= 10: #Se estiver chovendo os ventos serão forte, caso contrario normais
-            veloVen = round(random.triangular(20, 110, 20))
-        else:
-            veloVen = round(random.triangular(1, 40, 5))
-
-        costa = random.choice(ffT)
-        cidade = random.choice(ffT)
-        vegetacao = random.choice(ttF)
-        montanha = random.choice(ffT)
-
-        if random.random() < 0.05: #Cinco porcento de chance de não gerar um solo
-            solo = None
-        else:
-            solo = random.choice(solos)
-
-        if random.random() < 0.15:
-            nota = random.choice(notas)
-        else:
-            nota = None 
-        
-        if vazao > (mediaVazao*1.5): #Se a vazão estiver acima de 50% do normal
-            alagou = True
-            print('0')
-        elif miliHora > 35 and miliDia > 70:
-            alagou = True
-            print('1')
-        elif temp > 30 and montanha == True:
-            alagou = True
-            print('2')
-        elif miliHora > 60 and veloVen < 15:
-            alagou = True
-            print('3')
-        elif (solo == 'Arenoso' or 'ARENOSO' or 'arenoso') and vegetacao == False and miliHora > 40:
-            alagou = True
-            print('4')
-        elif mili7 > 400 and cidade == True:
-            alagou = True
-            print('5')
-        elif costa == True and nota == 'Mare alta':
-            alagou = True
-            print('6')
-        else:
-            alagou = False
-
-
-        datatemp = [idRio,mediaVazao,vazao,miliHora,miliDia,mili7,temp,veloVen,costa,cidade,vegetacao,montanha,solo,nota,alagou]
-        data.insert(idRio+1,datatemp)
-        print(datatemp)
-
-    with open('dadosAlagamentoPI.csv', 'w', newline='') as csvfile:
-        print('Escrito')
-        writer = csv.writer(csvfile)
-        writer.writerows(data)
